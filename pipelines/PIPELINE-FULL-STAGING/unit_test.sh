bc628a12ad10ff6fdd25614b8c1ca6333f6d25ec
Medenou Choumanof <rdaton@indra.es>
2023-02-06 17:43:45 +0100

    i

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -7,6 +7,6 @@ echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
-coverage run --include=src/todoList.py test/unit/TestToDo.py
+coverage run --include=src/todoList.py test/unit/TestToDo.py --cov-report term-missing
 coverage report
 coverage xml
